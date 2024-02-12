2f79265569714f7a9685656af19d6f4b59389702
Anderson Alvarez V <andersonav91@gmail.com>
2024-02-12 17:12:56 -0500

    First chart and table working

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,12 @@
 class TradesController < ApplicationController
   def index
-    @trades = Trade.where(params[:filters]).page(params[:page]).per(25)
+    @transaction_types = TransactionType.mapped_transaction_types
+
+    trades_filter = Trade.where(params[:filters])
+    @trades = trades_filter.page(params[:page]).per(25)
+    @statistics = {}
+    @statistics[:total_rows] = Hash[@transaction_types.map do |k, v|
+      [k.to_sym, trades_filter.where(transaction_type_id: v).count]
+    end]
   end
 end
