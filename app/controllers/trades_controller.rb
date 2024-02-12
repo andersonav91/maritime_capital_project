13e91ede9311c7854336c9091fdc9c3fd25e5104
Anderson Alvarez V <andersonav91@gmail.com>
2024-02-12 18:47:19 -0500

    Added statistics to the project

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -13,5 +13,13 @@ class TradesController < ApplicationController
     @statistics[:sum] = Hash[@transaction_types.map do |k, v|
       [k.to_sym, trades_filter.where(transaction_type_id: v).group(:transaction_type_id).sum(:notional_amount)]
     end]
+
+    @statistics[:notional_amount_sum] = trades_filter.sum(:notional_amount)
+
+    @statistics[:max_trade_date] = trades_filter.maximum(:trade_date)
+    @statistics[:min_trade_date] = trades_filter.minimum(:trade_date)
+
+    @statistics[:trade_price_avg] = trades_filter.average(:trade_price)
+    @statistics[:par_traded_avg] = trades_filter.average(:par_traded)
   end
 end
